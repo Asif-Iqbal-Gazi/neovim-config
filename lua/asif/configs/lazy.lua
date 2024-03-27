b850753a963cdcb0db65b5d19ce0d4291674a143
Asif Iqbal Gazi <Asif-Iqbal-Gazi@users.noreply.github.com>
2024-03-27 15:21:18 -0700

    Better catppuccin

--- lua/asif/configs/lazy.lua
+++ lua/asif/configs/lazy.lua
@@ -1,21 +1,21 @@
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
+  vim.fn.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
     lazypath,
-  }
+  })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup {
+require("lazy").setup({
   spec = LAZY_PLUGIN_SPEC,
-  install = { colorscheme = { "catppuccin" } },
+  install = { colorscheme = { "catppuccin", "default" } },
   ui = {
     border = "rounded",
     icons = {
@@ -29,4 +29,4 @@ require("lazy").setup {
     enabled = true,
     notify = false,
   },
-}
+})
--- lua/asif/plugins/colorscheme.lua
+++ lua/asif/plugins/colorscheme.lua
@@ -1,17 +1,18 @@
-local M = { "catppuccin/nvim", }
+local M = { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
 
 -- Configuration
 function M.config()
-  vim.cmd.colorscheme "catppuccin-mocha"
+  vim.cmd.colorscheme("default")
   require("catppuccin").setup({
-
+    flavour = "auto",
+    transparent_background = true,
     integrations = {
       cmp = true,
       nvimtree = true,
       treesitter = true,
       mason = true,
       indent_blankline = {
-        enabled = true,
+        enabled = false,
         scope_color = "green", -- catppuccin color (eg. `lavender`) Default: text
         colored_indent_levels = false,
       },
@@ -34,16 +35,18 @@ function M.config()
         },
       },
       navic = {
-        enabled = false,
+        enabled = true,
         custom_bg = "NONE", -- "lualine" will set background to mantle
       },
       telescope = {
         enabled = true,
         -- style = "nvchad"
       },
-      which_key = true
-    }
+      which_key = true,
+    },
   })
+
+  vim.cmd.colorscheme("catppuccin")
 end
 
 return M
