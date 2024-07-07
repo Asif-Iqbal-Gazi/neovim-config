793b0c09c9c956f031a49ed9d13f800d24b3dcd9
Asif-Iqbal-Gazi <Asif-Iqbal-Gazi@users.noreply.github.com>
2024-07-06 18:15:54 -0700

    Fixed clangd offsetEncoding in lspconfig

--- lua/asif/plugins/lspconfig.lua
+++ lua/asif/plugins/lspconfig.lua
@@ -1,135 +1,135 @@
 local M = {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = { { "folke/neodev.nvim" } },
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = { { "folke/neodev.nvim" } },
 }
 
 M.on_attach = function(client, bufnr)
-	local keymap = vim.api.nvim_buf_set_keymap
-	local opts = { noremap = true, silent = true }
-	keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  local keymap = vim.api.nvim_buf_set_keymap
+  local opts = { noremap = true, silent = true }
+  keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  keymap(bufnr, "n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  keymap(bufnr, "n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 
-	if client.supports_method("textDocument/inlayHint") then
-		vim.lsp.inlay_hint.enable(true, { bufnr })
-	end
+  if client.supports_method("textDocument/inlayHint") then
+    vim.lsp.inlay_hint.enable(true, { bufnr })
+  end
 end
 
 M.on_init = function(client, _)
-	if client.supports_method("textDocument/semanticTokens") then
-		client.server_capabilities.semanticTokensProvider = nil
-	end
+  if client.supports_method("textDocument/semanticTokens") then
+    client.server_capabilities.semanticTokensProvider = nil
+  end
 end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
-
+M.capabilities.offsetEncoding = { "utf-16" }
 M.capabilities.textDocument.completion.completionItem = {
-	documentationFormat = { "markdown", "plaintext" },
-	snippetSupport = true,
-	preselectSupport = true,
-	insertReplaceSupport = true,
-	labelDetailsSupport = true,
-	deprecatedSupport = true,
-	commitCharactersSupport = true,
-	tagSupport = { valueSet = { 1 } },
-	resolveSupport = {
-		properties = { "documentation", "detail", "additionalTextEdits" },
-	},
+  documentationFormat = { "markdown", "plaintext" },
+  snippetSupport = true,
+  preselectSupport = true,
+  insertReplaceSupport = true,
+  labelDetailsSupport = true,
+  deprecatedSupport = true,
+  commitCharactersSupport = true,
+  tagSupport = { valueSet = { 1 } },
+  resolveSupport = {
+    properties = { "documentation", "detail", "additionalTextEdits" },
+  },
 }
 
 M.toggle_inlay_hints = function()
-	local bufnr = vim.api.nvim_get_current_buf()
-	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr }), { bufnr })
+  local bufnr = vim.api.nvim_get_current_buf()
+  vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr }), { bufnr })
 end
 
 local servers = {
-	"html",
-	"clangd",
-	"lua_ls",
-	"jsonls",
-	"tsserver",
+  "html",
+  "clangd",
+  "lua_ls",
+  "jsonls",
+  "tsserver",
 }
 
 local wk_mappings = {
-	name = "LSP",
-	a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
-	f = {
-		"<cmd>lua vim.lsp.buf.format({async = true, filter = function(client) return client.name ~= 'typescript-tools' end})<cr>",
-		"Format",
-	},
-	i = { "<cmd>LspInfo<cr>", "LSP Info" },
-	j = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic" },
-	h = { "<cmd>lua require('asif.plugins.lspconfig').toggle_inlay_hints()<cr>", "Hints" },
-	k = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic" },
-	l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-	q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
-	r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+  name = "LSP",
+  a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+  f = {
+    "<cmd>lua vim.lsp.buf.format({async = true, filter = function(client) return client.name ~= 'typescript-tools' end})<cr>",
+    "Format",
+  },
+  i = { "<cmd>LspInfo<cr>", "LSP Info" },
+  j = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "Next Diagnostic" },
+  h = { "<cmd>lua require('asif.plugins.lspconfig').toggle_inlay_hints()<cr>", "Hints" },
+  k = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "Prev Diagnostic" },
+  l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+  q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
+  r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
 }
 
 function M.config()
-	local wk = require("which-key")
-	wk.register({ l = wk_mappings }, { prefix = "<leader>" })
+  local wk = require("which-key")
+  wk.register({ l = wk_mappings }, { prefix = "<leader>" })
 
-	local lspconfig = require("lspconfig")
-	local icons = require("asif.configs.icons")
+  local lspconfig = require("lspconfig")
+  local icons = require("asif.configs.icons")
 
-	local default_diagnostic_config = {
-		signs = {
-			active = true,
-			values = {
-				{ name = "DiagnosticSignError", text = icons.diagnostics.Error },
-				{ name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
-				{ name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
-				{ name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
-			},
-		},
-		virtual_text = false,
-		update_in_insert = false,
-		underline = true,
-		severity_sort = true,
-		float = {
-			focusable = true,
-			style = "minimal",
-			border = "rounded",
-			source = "always",
-			header = "",
-			prefix = "",
-		},
-	}
+  local default_diagnostic_config = {
+    signs = {
+      active = true,
+      values = {
+        { name = "DiagnosticSignError", text = icons.diagnostics.Error },
+        { name = "DiagnosticSignWarn",  text = icons.diagnostics.Warning },
+        { name = "DiagnosticSignHint",  text = icons.diagnostics.Hint },
+        { name = "DiagnosticSignInfo",  text = icons.diagnostics.Information },
+      },
+    },
+    virtual_text = false,
+    update_in_insert = false,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = true,
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+  }
 
-	vim.diagnostic.config(default_diagnostic_config)
+  vim.diagnostic.config(default_diagnostic_config)
 
-	for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
-		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
-	end
+  for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
+  end
 
-	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
-	vim.lsp.handlers["textDocument/signatureHelp"] =
-		vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
-	require("lspconfig.ui.windows").default_options.border = "rounded"
+  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+  vim.lsp.handlers["textDocument/signatureHelp"] =
+      vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+  require("lspconfig.ui.windows").default_options.border = "rounded"
 
-	for _, server in pairs(servers) do
-		local opts = {
-			on_attach = M.on_attach,
-			capabilities = M.capabilities,
-			on_init = M.on_init,
-		}
+  for _, server in pairs(servers) do
+    local opts = {
+      on_attach = M.on_attach,
+      capabilities = M.capabilities,
+      on_init = M.on_init,
+    }
 
-		local require_ok, settings = pcall(require, "asif.config.lspsettings." .. server)
-		if require_ok then
-			opts = vim.tbl_deep_extend("force", settings, opts)
-		end
+    local require_ok, settings = pcall(require, "asif.config.lspsettings." .. server)
+    if require_ok then
+      opts = vim.tbl_deep_extend("force", settings, opts)
+    end
 
-		if server == "lua_ls" then
-			require("neodev").setup({})
-		end
+    if server == "lua_ls" then
+      require("neodev").setup({})
+    end
 
-		lspconfig[server].setup(opts)
-	end
+    lspconfig[server].setup(opts)
+  end
 end
 
 return M
