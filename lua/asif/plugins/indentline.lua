5b2eece3ca9a725ec900e337e749771a5637289f
Asif Iqbal Gazi <Asif-Iqbal-Gazi@users.noreply.github.com>
2024-07-06 17:44:16 -0700

    Some fix in indentline and lspconfig

--- lua/asif/plugins/indentline.lua
+++ lua/asif/plugins/indentline.lua
@@ -1,31 +1,34 @@
 local M = {
-  "lukas-reineke/indent-blankline.nvim",
-  event = "BufReadPre",
+	"lukas-reineke/indent-blankline.nvim",
+	event = "BufReadPre",
 }
 
 function M.config()
-  local icons = require "asif.configs.icons"
+	local icons = require("asif.configs.icons")
+	local highlight = {
+		"RainbowRed",
+		"RainbowYellow",
+		"RainbowBlue",
+		"RainbowOrange",
+		"RainbowGreen",
+		"RainbowViolet",
+		"RainbowCyan",
+	}
 
-  local hl_name_list = {
-    "RainbowDelimiterRed",
-    "RainbowDelimiterYellow",
-    "RainbowDelimiterOrange",
-    "RainbowDelimiterGreen",
-    "RainbowDelimiterBlue",
-    "RainbowDelimiterCyan",
-    "RainbowDelimiterViolet",
-  }
+	local hooks = require("ibl.hooks")
+	-- create the highlight groups in the highlight setup hook, so they are reset
+	-- every time the colorscheme changes
+	hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+		vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+		vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+		vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+		vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+		vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+		vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+		vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+	end)
 
-  require("ibl").setup {
-    scope = {
-      enabled = true,
-      show_start = false,
-      highlight = hl_name_list,
-    },
-  }
-
-  local hooks = require "ibl.hooks"
-  hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+	require("ibl").setup({ indent = { highlight = highlight } })
 end
 
 return M
