e0f3a561514de98c6b086f34a31576401638acf0
Asif Iqbal Gazi <Asif-Iqbal-Gazi@users.noreply.github.com>
2025-01-20 16:41:18 -0800

    Add lazy-local.json to .gitignore

--- lua/asif/plugins/treesitter.lua
+++ lua/asif/plugins/treesitter.lua
@@ -1,27 +1,36 @@
 local M = {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-  build = ":TSUpdate",
-
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPost", "BufNewFile" },
+	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+	build = ":TSUpdate",
 }
 
 -- Define the languages for Tree-sitter
 local languages = {
-  "c", "cpp", "lua", "vim", "vimdoc", "html", "css",
-  "bash", "json", "java", "javascript", "python"
+	"c",
+	"cpp",
+	"lua",
+	"vim",
+	"vimdoc",
+	"html",
+	"css",
+	"bash",
+	"json",
+	"java",
+	"javascript",
+	"python",
 }
 
 -- Configuration
 function M.config()
-  require("nvim-treesitter.configs").setup {
-    ensure_installed = languages,
-    sync_install = false,
-    auto_install = true,
-    ignore_install = {},
-    highlight = { enable = true },
-    indent = { enable = true },
-  }
+	require("nvim-treesitter.configs").setup({
+		ensure_installed = languages,
+		sync_install = false,
+		auto_install = true,
+		ignore_install = {},
+		highlight = { enable = true },
+		indent = { enable = true },
+	})
 end
 
 return M
