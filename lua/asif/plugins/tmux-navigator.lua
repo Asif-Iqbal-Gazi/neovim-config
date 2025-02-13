a241146eaf5622fb1e01ffaf937a323114ae2aef
Asif Iqbal Gazi <Asif-Iqbal-Gazi@users.noreply.github.com>
2025-02-12 23:33:17 -0800

    Tmux navigator

---
+++ lua/asif/plugins/tmux-navigator.lua
@@ -0,0 +1,23 @@
+local M = {
+  "christoomey/vim-tmux-navigator",
+  event = "VeryLazy",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
+
+function M.config() end
+
+return M
