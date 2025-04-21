8236ee82e499d12d33c8dd63c1c788f7e6bab5e7
Asif Iqbal Gazi <Asif-Iqbal-Gazi@users.noreply.github.com>
2025-04-21 01:10:04 -0700

    formatted options.lua

--- lua/asif/configs/options.lua
+++ lua/asif/configs/options.lua
@@ -1,91 +1,113 @@
 local g = vim.g
 local opt = vim.opt
 
--------------------------------------- globals -----------------------------------------
--- Disable netrw at the very start
+--------------------------------------------------------------------------------
+-- 📦 Plugin & Built-in Behavior
+--------------------------------------------------------------------------------
+
+-- Disable netrw (useful if using a modern file explorer like nvim-tree)
 g.loaded_netrw = 1
 g.loaded_netrwPlugin = 1
+g.netrw_banner = 0         -- Hide netrw banner
+g.netrw_mouse = 2          -- Enable mouse support in netrw
 
+--------------------------------------------------------------------------------
+-- 🧭 UI Settings
+--------------------------------------------------------------------------------
 
--------------------------------------- options -----------------------------------------
--- Status Line
-opt.laststatus = 3
-opt.showmode = false
+-- Status line
+opt.laststatus = 3         -- Global statusline
+opt.showmode = false       -- Don't show mode (handled by statusline plugin)
 
--- Clipboard
-opt.clipboard = "unnamedplus"
+-- Line numbers
+opt.number = true
+opt.relativenumber = true
+opt.numberwidth = 2
+opt.ruler = false          -- Disable old ruler display
 
 -- Cursor
 opt.cursorline = true
-opt.cursorlineopt = "both"
+opt.cursorlineopt = "both" -- Highlight both number and line
 
--- Indenting
-opt.expandtab = true
-opt.shiftwidth = 2
-opt.smartindent = true
-opt.tabstop = 2
-opt.softtabstop = 2
+-- Sign column
+opt.signcolumn = "yes"     -- Always show sign column
 
 -- Fill characters
-opt.fillchars = { eob = " " }
+opt.fillchars = { eob = " " } -- Remove ~ at end of buffer
 
--- Search
-opt.ignorecase = true
-opt.smartcase = true
+-- GUI
+opt.termguicolors = true   -- Enable true color support
+
+-- Scroll
+opt.scrolloff = 10         -- Minimum lines above/below cursor
+
+--------------------------------------------------------------------------------
+-- 🧠 Behavior & UX
+--------------------------------------------------------------------------------
 
 -- Mouse
-opt.mouse = "a"
+opt.mouse = "a"            -- Enable mouse in all modes
 
--- Numbers
-opt.number = true
-opt.numberwidth = 2
-opt.relativenumber = true
-opt.ruler = false
-
--- Nvim Intro
-opt.shortmess:append "sI"
-
--- Sign Column
-opt.signcolumn = "yes"
-
--- Splitting
+-- Splits
 opt.splitbelow = true
 opt.splitright = true
 
--- Timeout
-opt.timeoutlen = 400
-
--- Undo
-opt.undofile = true
-
--- Update time
-opt.updatetime = 100
-
--- Which Wrap
-opt.whichwrap:append "<>[]hl"
-
--- GUI Colors
-opt.termguicolors = true
-
--- Backup
-opt.writebackup = false
+-- Timeouts
+opt.timeoutlen = 400       -- Faster mappings
 
 -- Completion
 opt.completeopt = { "menuone", "noselect" }
 
--- Popup Menu
+-- Popup menu
 opt.pumheight = 10
-opt.pumblend = 10
+opt.pumblend = 10          -- Transparency
 
--- Swapfile
-opt.swapfile = false
+-- Short messages
+opt.shortmess:append "sI"  -- Disable intro message
 
--- File Encoding
--- opt.fileencoding = "utf-8" -- uncomment if needed
+-- Word navigation
+opt.whichwrap:append "<>[]hl" -- Move to next/prev line with h/l at start/end
 
--- Additional Settings
-vim.cmd [[set iskeyword+=-]]
+-- Clipboard
+opt.clipboard = "unnamedplus" -- Sync with system clipboard
 
--- Netrw
-g.netrw_banner = 0
-g.netrw_mouse = 2
+-- Keyword definition
+vim.cmd [[set iskeyword+=-]]  -- Treat dash-separated words as one
+
+--------------------------------------------------------------------------------
+-- 🔍 Search
+--------------------------------------------------------------------------------
+
+opt.ignorecase = true      -- Case insensitive search...
+opt.smartcase = true       -- ...unless uppercase is used
+
+--------------------------------------------------------------------------------
+-- ✍️ Editing & Formatting
+--------------------------------------------------------------------------------
+
+-- Indentation
+opt.expandtab = true       -- Use spaces instead of tabs
+opt.shiftwidth = 4         -- Size of an indent
+opt.tabstop = 4            -- Number of spaces tabs count for
+opt.softtabstop = 4
+opt.smarttab = true
+opt.smartindent = true
+opt.autoindent = true
+opt.breakindent = true
+
+-- Whitespace characters
+opt.list = true
+opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+--------------------------------------------------------------------------------
+-- 💾 Files & Backups
+--------------------------------------------------------------------------------
+
+opt.swapfile = false       -- Disable swapfile
+opt.writebackup = false    -- Disable write backup
+opt.undofile = true        -- Enable persistent undo
+opt.updatetime = 100       -- Faster updates (good for plugins)
+
+-- File encoding (uncomment if needed)
+-- opt.fileencoding = "utf-8"
+
