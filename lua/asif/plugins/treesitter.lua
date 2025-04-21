40e49b1d301bfdba3d706da330d2da4100d7d2cc
Asif Iqbal Gazi <Asif-Iqbal-Gazi@users.noreply.github.com>
2025-04-21 01:06:54 -0700

    updated treesitter according latest lazy doc

--- lua/asif/plugins/treesitter.lua
+++ lua/asif/plugins/treesitter.lua
@@ -1,39 +1,49 @@
 local M = {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo", "TSUpdate" },
-  build = ":TSUpdate",
-}
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo", "TSUpdate" },
+    build = ":TSUpdate",
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    opts = {
+        ensure_installed = {
+            "c",
+            "cpp",
+            "lua",
+            "vim",
+            "vimdoc",
+            "html",
+            "css",
+            "bash",
+            "json",
+            "java",
+            "javascript",
+            "python",
+            "markdown",
+            "markdown_inline",
 
--- Define the languages for Tree-sitter
-local languages = {
-  "c",
-  "cpp",
-  "lua",
-  "vim",
-  "vimdoc",
-  "html",
-  "css",
-  "bash",
-  "json",
-  "java",
-  "javascript",
-  "python",
-  "markdown",
-  "markdown_inline",
+        },
+        sync_install = false,
+        -- Autoinstall languages that are not installed
+        auto_install = true,
+        ignore_install = {},
+        highlight = {
+            enable = true,
+            -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+            --  If you are experiencing weird indenting issues, add the language to
+            --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+            additional_vim_regex_highlighting = { "ruby" },
+        },
+        indent = { enable = true, disable = { "ruby" } },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = "<CR>",
+                scope_incremental = "<CR>",
+                node_incremental = "<TAB>",
+                node_decremental = "<S-TAB>",
+            },
+        },
+    }
 }
-
--- Configuration
-function M.config()
-  local treesitter_configs = require("nvim-treesitter.configs")
-  treesitter_configs.setup({
-    ensure_installed = languages,
-    sync_install = false,
-    auto_install = true,
-    ignore_install = {},
-    highlight = { enable = true },
-    indent = { enable = true },
-  })
-end
 
 return M
