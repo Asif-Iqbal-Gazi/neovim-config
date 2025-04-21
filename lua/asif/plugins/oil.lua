896e04f438cc57da50f4cfdda7a1905107b77ed4
Asif Iqbal Gazi <Asif-Iqbal-Gazi@users.noreply.github.com>
2025-04-21 12:41:14 -0700

    Moved oil related keymap to oil.lua file

--- lua/asif/plugins/oil.lua
+++ lua/asif/plugins/oil.lua
@@ -1,13 +1,16 @@
 local M = {
     'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
     -- Optional dependencies
     -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    keys = {
+        { "-", "<cmd>Oil --float<cr>", desc = "Open Parent Directory in Oil" },
+    }
 }
 
 return M
