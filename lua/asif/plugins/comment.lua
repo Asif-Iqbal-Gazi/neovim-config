c8782d651d78064bdb72783e4ec68ffa5756184e
Asif Iqbal Gazi <Asif-Iqbal-Gazi@users.noreply.github.com>
2025-04-21 18:49:56 -0700

    Removed nvim-ts-context dependency

--- lua/asif/plugins/comment.lua
+++ lua/asif/plugins/comment.lua
@@ -1,28 +1,9 @@
 local M = {
-  "numToStr/Comment.nvim",
-  dependencies = {
-    {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-      event = "VeryLazy",
-    },
-  },
+    "numToStr/Comment.nvim",
+    keys = {
+        { "<leader>/", "<Plug>(comment_toggle_linewise_current)", desc = "Comment", mode = "n" },
+        { "<leader>/", "<Plug>(comment_toggle_linewise_visual)",  desc = "Comment", mode = "v" }
+    }
 }
-
-function M.config()
-  local wk = require("which-key")
-
-  wk.add({ { "<leader>/", "<Plug>(comment_toggle_linewise_current)", desc = "Comment", mode = "n" } })
-  wk.add({ { "<leader>/", "<Plug>(comment_toggle_linewise_visual)", desc = "Comment", mode = "v" } })
-
-  vim.g.skip_ts_context_commentstring_module = true
-  ---@diagnostic disable: missing-fields
-  require("ts_context_commentstring").setup({
-    enable_autocmd = false,
-  })
-
-  require("Comment").setup({
-    pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-  })
-end
 
 return M
