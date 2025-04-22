7f1fc5ccac4a25313b9ca6d9d911a309f1805c75
Asif-Iqbal-Gazi <Asif-Iqbal-Gazi@users.noreply.github.com>
2025-04-21 21:01:30 -0700

    Added vim.o.winborder

--- lua/asif/configs/options.lua
+++ lua/asif/configs/options.lua
@@ -8,87 +8,95 @@ local opt = vim.opt
 -- Disable netrw (useful if using a modern file explorer like nvim-tree)
 g.loaded_netrw = 1
 g.loaded_netrwPlugin = 1
-g.netrw_banner = 0         -- Hide netrw banner
-g.netrw_mouse = 2          -- Enable mouse support in netrw
+g.netrw_banner = 0 -- Hide netrw banner
+g.netrw_mouse = 2  -- Enable mouse support in netrw
 
 --------------------------------------------------------------------------------
 -- 🧭 UI Settings
 --------------------------------------------------------------------------------
 
 -- Status line
-opt.laststatus = 3         -- Global statusline
-opt.showmode = false       -- Don't show mode (handled by statusline plugin)
+opt.laststatus = 3   -- Global statusline
+opt.showmode = false -- Don't show mode (handled by statusline plugin)
 
 -- Line numbers
 opt.number = true
 opt.relativenumber = true
 opt.numberwidth = 2
-opt.ruler = false          -- Disable old ruler display
+opt.ruler = false -- Disable old ruler display
 
 -- Cursor
 opt.cursorline = true
 opt.cursorlineopt = "both" -- Highlight both number and line
 
 -- Sign column
-opt.signcolumn = "yes"     -- Always show sign column
+opt.signcolumn = "yes" -- Always show sign column
 
 -- Fill characters
 opt.fillchars = { eob = " " } -- Remove ~ at end of buffer
 
 -- GUI
-opt.termguicolors = true   -- Enable true color support
+opt.termguicolors = true -- Enable true color support
 
 -- Scroll
-opt.scrolloff = 10         -- Minimum lines above/below cursor
+opt.scrolloff = 10 -- Minimum lines above/below cursor
 
 --------------------------------------------------------------------------------
 -- 🧠 Behavior & UX
 --------------------------------------------------------------------------------
 
 -- Mouse
-opt.mouse = "a"            -- Enable mouse in all modes
+opt.mouse = "a" -- Enable mouse in all modes
 
 -- Splits
 opt.splitbelow = true
 opt.splitright = true
 
 -- Timeouts
-opt.timeoutlen = 400       -- Faster mappings
+opt.timeoutlen = 400 -- Faster mappings
 
 -- Completion
 opt.completeopt = { "menuone", "noselect" }
 
 -- Popup menu
 opt.pumheight = 10
-opt.pumblend = 10          -- Transparency
+opt.pumblend = 10 -- Transparency
 
 -- Short messages
-opt.shortmess:append "sI"  -- Disable intro message
+opt.shortmess:append("sI") -- Disable intro message
 
 -- Word navigation
-opt.whichwrap:append "<>[]hl" -- Move to next/prev line with h/l at start/end
+opt.whichwrap:append("<>[]hl") -- Move to next/prev line with h/l at start/end
 
 -- Clipboard
 opt.clipboard = "unnamedplus" -- Sync with system clipboard
 
 -- Keyword definition
-vim.cmd [[set iskeyword+=-]]  -- Treat dash-separated words as one
+-- vim.cmd([[set iskeyword+=-]]) -- Treat dash-separated words as one
+opt.iskeyword:append("-") -- Treat dash-separated words as one
+
+--------------------------------------------------------------------------------
+-- 🌐 vim.o Global-only Settings
+--------------------------------------------------------------------------------
+
+vim.o.winborder = "rounded" -- Rounded borders for floating windows
+--vim.o.winhighlight = "Normal:Normal,FloatBorder:FloatBorder" -- Cleaner float UI (optional)
 
 --------------------------------------------------------------------------------
 -- 🔍 Search
 --------------------------------------------------------------------------------
 
-opt.ignorecase = true      -- Case insensitive search...
-opt.smartcase = true       -- ...unless uppercase is used
+opt.ignorecase = true -- Case insensitive search...
+opt.smartcase = true  -- ...unless uppercase is used
 
 --------------------------------------------------------------------------------
 -- ✍️ Editing & Formatting
 --------------------------------------------------------------------------------
 
 -- Indentation
-opt.expandtab = true       -- Use spaces instead of tabs
-opt.shiftwidth = 4         -- Size of an indent
-opt.tabstop = 4            -- Number of spaces tabs count for
+opt.expandtab = true -- Use spaces instead of tabs
+opt.shiftwidth = 4   -- Size of an indent
+opt.tabstop = 4      -- Number of spaces tabs count for
 opt.softtabstop = 4
 opt.smarttab = true
 opt.smartindent = true
@@ -103,11 +111,10 @@ opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 -- 💾 Files & Backups
 --------------------------------------------------------------------------------
 
-opt.swapfile = false       -- Disable swapfile
-opt.writebackup = false    -- Disable write backup
-opt.undofile = true        -- Enable persistent undo
-opt.updatetime = 100       -- Faster updates (good for plugins)
+opt.swapfile = false    -- Disable swapfile
+opt.writebackup = false -- Disable write backup
+opt.undofile = true     -- Enable persistent undo
+opt.updatetime = 100    -- Faster updates (good for plugins)
 
 -- File encoding (uncomment if needed)
 -- opt.fileencoding = "utf-8"
-
