9b3c96c6159c45175ae2e853cc84249633833394
Asif Iqbal Gazi <Asif-Iqbal-Gazi@users.noreply.github.com>
2025-04-24 14:30:32 -0700

    Added option to view hiddle_files in oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,16 +1,18 @@
 local M = {
-    "stevearc/oil.nvim",
-    -- Optional dependencies
-    -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    lazy = false,
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
-    keys = {
-        { "-", "<cmd>Oil --float<cr>", desc = "Open Parent Directory in Oil" },
-    },
+	"stevearc/oil.nvim",
+	-- Optional dependencies
+	-- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		view_options = { show_hidden = true },
+	},
+	keys = {
+		{ "-", "<cmd>Oil --float<cr>", desc = "Open Parent Directory in Oil" },
+	},
 }
 
 return M
